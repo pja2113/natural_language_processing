b0cad237ad532449132af1f033eb539d6093c1a6
pja2113 <161179215+pja2113@users.noreply.github.com>
2025-07-15 15:52:56 -0400

    Init commit

---
+++ custom_utility/utils.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  8 18:09:29 2025
+
+@author: Philip Auerbach
+"""
+# NLP RELEVANT FUNCTIONS 
+
+# Function: Jaccard Similarity (String)
+# Purpose:  Measure of how similar two strings are
+# Input: Two strings to compare similarity
+# Output: float - score between 0 and 1 (inclusive)
+
+def jd_sim_string(str_a, str_b):
+    tok_a = str_a.split()
+    tok_b = str_b.split()
+    set_a = set(tok_a)
+    set_b = set(tok_b)
+    inter_tmp = set_a.intersection(set_b)
+    union_tmp = set_a.union(set_b)
+    jd_tmp = len(inter_tmp) / len(union_tmp)
+    return jd_tmp
+
+# Function: Adder
+# Purpose:  Add two values with basic error handling
+# Input: Two values to add (a_in, b_in)
+# Output: Sum if successful, else None
+
+def adder(a_in, b_in):
+    tmp = None
+    try:
+        tmp = a_in + b_in
+    except:
+        print ("can't add", a_in, b_in)
+        pass
+    return tmp
+
+# Function: Word Frequency
+# Purpose:  Count frequency of words in a string
+# Input: A string
+# Output: Dictionary of word counts
+
+def wrd_freq(c_in):
+    tmp = c_in.split()
+    t_d = dict()
+    for word in set(tmp):
+        t_d[word] = tmp.count(word)
+    return t_d
+
+
+# Function: Word Frequency (Redux)
+# Purpose:  Efficiently count frequency of words in a string
+# Input: A string
+# Output: Dictionary of word counts
+
+def wrd_freq_redux(str_in):
+    import collections
+    the_new_ans = dict(collections.Counter(str_in.split()))
+    return the_new_ans
+
+# Function: Clean Text
+# Purpose:  Remove non-letter characters, lowercase text
+# Input: A string
+# Output: Cleaned string
+
+def clean_text(str_in):
+    import re
+    cln_txt = re.sub(
+        "[^A-Za-z']+", " ", str_in).strip().lower()
+    return cln_txt
+
+# Function: File Opener
+# Purpose:  Open a file, read and clean its contents
+# Input: File path as a string
+# Output: Cleaned text from file or empty string on failure
+
+def file_opener(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            return clean_text(f.read())
+    except:
+        print ("Can't open", file_path)
+        return ''
+        pass
+
+# Function: File Crawler
+# Purpose:  Recursively read and label text files in a directory
+# Input: Directory path
+# Output: Pandas DataFrame with 'body' and 'label' columns
+
+def file_crawler(p_in):
+    import pandas as pd
+    import os
+    tmp_d = pd.DataFrame()
+    for root, dirs, files in os.walk(p_in, topdown=False):
+       for name in files:
+          tmp = root + "/" + name
+          t_txt = file_opener(tmp)
+          if len(t_txt) != 0:
+          #if t_txt is not None: 
+              t_dir = root.split("/")[-1]
+              tmp_pd = pd.DataFrame(
+                  {"body": t_txt, "label": t_dir}, index=[0])
+              tmp_d = pd.concat([tmp_d, tmp_pd], ignore_index=True)
+    return tmp_d
+
+# Function: Token Counter
+# Purpose:  Count total or unique tokens in text
+# Input: Text string, mode ("all" or anything else for unique)
+# Output: Integer token count
+
+def tok_cnt(c_in, sw_in):
+    tmp = c_in.split()
+    if sw_in == "all":
+        return len(tmp)
+    else:
+        return len(set(tmp))
+
+# Function: Word Statistics
+# Purpose:  Compute total and unique token counts by topic
+# Input: DataFrame with 'label' and 'body' columns
+# Output: Dictionary with counts for each topic
+
+def wrd_fun(df_in):
+    combined_t = dict()
+    for topic in df_in["label"].unique():
+        tmp = df_in[df_in["label"] == topic]
+        tmp = tmp["body"].str.cat(sep= " ")
+        tmp = tmp.split()
+        tmp_all = len(tmp)
+        tmp_u = len(set(tmp))
+        combined_t[topic] = {"all": tmp_all, "unique": tmp_u}
+        # combined = the_data.groupby('label', as_index=False).agg({'body': ' '.join})
+        # combined['num_token'] = combined['body'].apply(lambda x: tok_cnt(x,"all"))
+        # combined['num_token_unique'] = combined['body'].apply(lambda x: tok_cnt(x,"u"))
+        # result_dict = {}
+        # for _,row in combined.iterrows():
+        #     result_dict[row['label']] = {
+        #         'all': row['num_token'],
+        #         'unique': row['num_token_unique']
+        #     }
+    return combined_t
+
+
+# Function: Remove Stopwords
+# Purpose:  Remove English stopwords from a string
+# Input: A string
+# Output: Cleaned string with stopwords removed
+    
+def rem_sw(text):
+    from nltk.corpus import stopwords
+    sw = stopwords.words('english')
+    return ' '.join([word for word in text.split() if word.lower() not in sw])
+
+#LECTURE SPECIFIC FUNCTIONS 
+
+# Function: Word Counter by Topic
+# Purpose:  Count individual word occurrences by topic
+# Input: DataFrame with 'label' and 'body' columns
+# Output: Dictionary of word Counter objects per topic
+
+def word_fun(df_in):
+    import collections
+    wrd_cnt_fun = dict()
+    for topic in df_in["label"].unique():
+        tmp = df_in[df_in["label"] == topic]
+        tmp = tmp["body"].str.cat(sep=" ")
+        wrd_cnt_fun[topic] = collections.Counter(tmp.split())
+    return wrd_cnt_fun
+
+
+
+# ALTERNATIVE EFFICIENCY FUNCTIONS 
+
+
+# def rem_sw(str_in):
+#     n_l = list()
+#     from nltk.corpus import stopwords
+#     sw = stopwords.words('english')
+#     # n_l = [word for word in t_c.split() if word not in sw]
+#     # n_l = " ".join(n_l)
+#     for word in str_in.split():
+#         if word not in sw:
+#             n_l.append(word)
+#     n_l = " ".join(n_l)
+#     return n_l
+
+# def file_opener(p_in):
+#     try:
+#         txt = ''
+#         #txt = None
+#         f = open(p_in, "r", encoding="UTF8")
+#         txt = f.read()
+#         txt = clean_text(txt)
+#         f.close()
+#     except:
+#         print ("Can't open", p_in)
+#         pass
+#     return txt
---
+++ lectures.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  7 18:57:07 2025
+
+@author: Philip Auerbach
+"""
+
+#from utils import jd_fun, hello_world
+from utils import *
+the_path = "C:/Users/pathouli/Box Sync/myStuff/academia/columbia/socialSciences/GR5067/2025_summer/output/"
+the_d_path = "C:/Users/pathouli/Box Sync/myStuff/academia/columbia/socialSciences/GR5067/2025_summer/data/"
+
+the_data = file_crawler(the_d_path)
+
+"""
+create a function called tok_cnt that accepts a corpus
+outputs the number of tokens
+"""
+
+the_data["num_tok"] = the_data["body"].apply(
+    lambda x: tok_cnt(x, "all"))
+the_data["num_tok_u"] = the_data["body"].apply(
+    lambda x: tok_cnt(x, "u"))
+
+the_data["body_sw"] = the_data["body"].apply(rem_sw)
+
+"""
+Create dictionary where the key is the topic
+the value is a dictionary that has 2 keys:
+    all which is the total number of tokens
+    unique which is the unique number of tokens
+    
+test_dict = {"fishing": {"all": 344, "unqiue": 290},
+             "mathematics": {"all": 434, "unqiue": 321}}
+"""
+
+#tmp = the_data[the_data["label"] == "fishing"]
+# tmp_concat = tmp["body"].str.cat(sep=" ")
+
+# combined = wrd_fun(the_data)
+
+"""
+create a dictionary where the key is a unique topic
+and the value is the word frequency dictionary key:count
+"""
+
+# a = ["the", "cat", "ran", "up", "the", "hill"]
+
+# a_filt = list()
+# for word in a:
+#     if word != "the":
+#         a_filt.append(word)     
+# corp = " ".join(a_filt) #package up a list into a single string/corpus
+
+# a_filt_b = [word for word in a if word != "the"] #list comprehension
+# corp_a = " ".join(a_filt_b) 
+
+# #dictionary comprehension
+# a_dict = dict()
+# for word in set(a):
+#     a_dict[word] = len(word)
+
+# a = ["the", "cat", "ran", "up", "the", "hill"]
+# a_dict = {word:len(word) for word in set(a)}
+
+
+
+
+"""
+create a function called rem_sw that removes
+all stopwords and returns everything else
+ex input: cat in the hat
+ex output: cat hat
+"""
+
+#t_c = "cat in the hat"
+#print (rem_sw("cat in the hat"))
+
+#inline
+
+#test = word_fun(the_data)
+#print (tok_cnt("cat cat hat", "u"))
+
+#def tok_cnt_u(c_in):
+#    tmp = c_in.split()
+#    tmp = len(set(tmp))
+#    return tmp
+
+#the_data["num_tok"] = the_data["body"].apply(tok_cnt)
+#the_data["num_tok_u"] = the_data["body"].apply(tok_cnt_u)
+
+#sum_stats = the_data.describe()
+
+"""
+create a function called tok_cnt_u that accepts a corpus
+outputs the number of unique tokens 
+"""
+
+
+#summary statistics
+#sum_stats = the_data.describe() #good summary statistics for data section
+#specific stats
+#avg = the_data["num_tok"].std()
+
+#def tok_cnt(c_in):
+#    return len(c_in.split())
+
+"""
+create a function that accepts a path and file name of a .txt file
+and outputs the contents of the file, make sure to run it through
+clean_text
+"""
+
+"""
+refactor the code to NOT include any processed corpus that has 
+no tokens, i.e. its blank, ie empty
+"""
+
+#cover IFs than revisit
+# a = 1
+# b = 4
+# c = 10
+
+# if a > 1:
+#     print ("hello", a)
+# elif b == 3:
+#     print ("hello", b)
+# elif c == 10:
+#     print ("hello", c)
+# elif c < 10:
+#     print ("hello", c)
+# else:
+#     print ("Hello World")
+
+# if a > 1:
+#     print ("hello", a)
+# if b == 3:
+#     print ("hello", b)
+# if c == 10:
+#     print ("hello", c)
+# if c < 10:
+#     print ("hello", c)
+# else:
+#     print ("Hello World")
+
+
+
+
+
